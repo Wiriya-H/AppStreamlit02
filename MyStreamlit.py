5d4512b4d66047a477ba1340b9a58007862bf515
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-09-26 14:59:18 +0700

    .commit

--- MyStreamlit.py
+++ MyStreamlit.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+st.image('./pic/1.jpg')
+col1,col2 = st.columns(2)
+
+col1 with:
+    st.header('วิริยะ เหมมาลา')
+col2 with:
+    st.subheader('สาขาวิทยาการข้อมูล')
+
 st.header('Wiriya Hemmala')
