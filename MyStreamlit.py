b3c390172b099f106e385385d0c52e2ce7b0b8b9
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-09-26 15:18:17 +0700

    .commit

--- MyStreamlit.py
+++ MyStreamlit.py
@@ -10,7 +10,7 @@ with col2:
     st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
 
 html_1 = """
-<div style="background-color:97FFF4;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#97FFF4;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้เบื้องต้น</h5></center>
 </div>
 """
