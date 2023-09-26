665f924c6aa6e7a315288783678dd4abec044a6e
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-09-26 15:08:50 +0700

    .commit

--- MyStreamlit.py
+++ MyStreamlit.py
@@ -9,4 +9,10 @@ with col2:
     st.subheader('สาขาวิทยาการข้อมูล')
     st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
 
-st.header('Wiriya Hemmala')
+html_1 = """
+<div style="background-color:#E67E22;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้เบื้องต้น</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
